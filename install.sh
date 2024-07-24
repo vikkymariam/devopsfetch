25931aab001547e4b36856ed2185974f3493058d
vickymariam <vickymariam50@gmail.com>
2024-07-25 00:38:01 +0100

    made some corrections to the install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,54 @@
+# #!/bin/bash
+
+# # Install necessary dependencies
+# sudo apt update
+# sudo apt install -y net-tools docker.io nginx
+
+# # Set up systemd service
+# SERVICE_FILE=/etc/systemd/system/devopsfetch.service
+# echo "[Unit]
+# Description=DevOps Fetch Service
+
+# [Service]
+# ExecStart=$PWD/devopsfetch.sh -t 1h
+# Restart=always
+# User=$(whoami)
+
+# [Install]
+# WantedBy=multi-user.target" | sudo tee $SERVICE_FILE
+
+# # Enable and start the service
+# sudo systemctl enable devopsfetch.service
+# sudo systemctl start devopsfetch.service
+
+# # Set up log rotation
+# LOGROTATE_FILE=/etc/logrotate.d/devopsfetch
+# echo "$PWD/logs/*.log {
+#     daily
+#     missingok
+#     rotate 14
+#     compress
+#     notifempty
+#     create 0640 $(whoami) $(whoami)
+#     sharedscripts
+#     postrotate
+#         systemctl reload devopsfetch.service > /dev/null 2>/dev/null || true
+#     endscript
+# }" | sudo tee $LOGROTATE_FILE
+
+# echo "Installation complete. DevOps Fetch service is now running."
+
 #!/bin/bash
 
 # Install necessary dependencies
 sudo apt update
 sudo apt install -y net-tools docker.io nginx
 
-# Set up systemd service
-SERVICE_FILE=/etc/systemd/system/devopsfetch.service
-echo "[Unit]
-Description=DevOps Fetch Service
+# Create a log directory if it does not exist
+mkdir -p /mnt/c/Users/USER/Documents/HNG/devopsfetch/logs
 
-[Service]
-ExecStart=$PWD/devopsfetch.sh -t 1h
-Restart=always
-User=$(whoami)
+# Run the script manually in the background
+nohup /mnt/c/Users/USER/Documents/HNG/devopsfetch/devopsfetch.sh -t 1h > /mnt/c/Users/USER/Documents/HNG/devopsfetch/devopsfetch.log 2>&1 &
 
-[Install]
-WantedBy=multi-user.target" | sudo tee $SERVICE_FILE
+echo "Installation complete. DevOps Fetch script is now running in the background."
 
-# Enable and start the service
-sudo systemctl enable devopsfetch.service
-sudo systemctl start devopsfetch.service
-
-# Set up log rotation
-LOGROTATE_FILE=/etc/logrotate.d/devopsfetch
-echo "$PWD/logs/*.log {
-    daily
-    missingok
-    rotate 14
-    compress
-    notifempty
-    create 0640 $(whoami) $(whoami)
-    sharedscripts
-    postrotate
-        systemctl reload devopsfetch.service > /dev/null 2>/dev/null || true
-    endscript
-}" | sudo tee $LOGROTATE_FILE
-
-echo "Installation complete. DevOps Fetch service is now running."
